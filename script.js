151045a8636dfdcad764273a78d81446e23eb958
Remy <remy@chaoticmewtral.com>
2022-11-16 17:53:35 -0500

    Add to directory

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-const qty = document.querySelector('#quantity');
+const quantity = document.querySelector('#quantity');
+const delivery = document.querySelector('#delivery');
+const local = document.querySelector('#pickup')
+const form = document.querySelector('form');
 
+quantity.addEventListener('change', updatePrice);
+delivery.addEventListener('input', addAddressField);
+local.addEventListener('input', addAddressField);
+
+
+function updatePrice() {
+    const qty = document.querySelector('#qty');
+    const subtotal = document.querySelector('#subtotal');
+    qty.textContent = quantity.value;
+    subtotal.textContent = quantity.value;
+}
+
+function addAddressField() {
+    const address = document.querySelector('#addy');
+    if (delivery.checked) {
+        address.style.display = 'inline-block';
+    } else if (local.checked) {
+        address.style.display = 'none';
+    }
+}
